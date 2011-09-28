6ccfbbbf2e0c7c2bbd0699f13b306732446b8197
dvir volk <dvir@doit9.com>
2011-09-29 00:01:13 +0300

    Added support for HOME and END keys

--- linenoise.c
+++ linenoise.c
@@ -387,7 +387,21 @@ right_arrow:
                     pos++;
                     refreshLine(fd,prompt,buf,len,pos,cols);
                 }
-            } else if (seq[0] == 91 && (seq[1] == 65 || seq[1] == 66)) {
+            } else if (seq[0] == 79 && seq[1] == 72) {
+                 /* home button */
+                 if (pos > 0) {
+                     pos = 0;
+                     refreshLine(fd,prompt,buf,len,pos,cols);
+                 }
+             } else if (seq[0] == 79 && seq[1] == 70) {
+                 /* end button */
+                 if (pos != len) {
+                     pos = len;
+                     refreshLine(fd,prompt,buf,len,pos,cols);
+                 }
+             }
+
+             else if (seq[0] == 91 && (seq[1] == 65 || seq[1] == 66)) {
 up_down_arrow:
                 /* up and down arrow: history */
                 if (history_len > 1) {
