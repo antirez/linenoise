69dab1fa02d595ee1074bcf844e535d0f50b91aa
Marek Marecki <marekjm@ozro.pw>
2015-04-04 01:51:15 +0200

    Fix: unsupported conversion from string constant

--- linenoise.c
+++ linenoise.c
@@ -118,7 +118,7 @@
 
 #define LINENOISE_DEFAULT_HISTORY_MAX_LEN 100
 #define LINENOISE_MAX_LINE 4096
-static char *unsupported_term[] = {"dumb","cons25","emacs",NULL};
+static const char *unsupported_term[] = {"dumb","cons25","emacs",NULL};
 static linenoiseCompletionCallback *completionCallback = NULL;
 
 static struct termios orig_termios; /* In order to restore at exit.*/
