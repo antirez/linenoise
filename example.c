ca5819c995a86d3924a0746573b0449ba71541f6
Marek Marecki <marekjm@ozro.pw>
2015-04-04 01:45:49 +0200

    In example: quit does not print

--- example.c
+++ example.c
@@ -52,12 +52,12 @@ int main(int argc, char **argv) {
     while((line = linenoise(">>> ")) != NULL) {
         /* Do something with the string. */
         if (line[0] != '\0' && line[0] != '/') {
-            printf("echo: '%s'\n", line);
             linenoiseHistoryAdd(line); /* Add to the history. */
             linenoiseHistorySave("history.txt"); /* Save the history on disk. */
             if (strcmp(line, "quit") == 0) {
                 break;
             }
+            printf("echo: '%s'\n", line);
         } else if (!strncmp(line,"/historylen",11)) {
             /* The "/historylen" command will change the history len. */
             int len = atoi(line+11);
