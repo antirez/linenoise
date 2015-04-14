6f1b28779ad26004d4e0e478b6b56b4422c6ea93
Alex Gray <alex@mrgray.com>
2015-04-14 04:49:39 -0400

    reset to use same history.txt as c emxample, obviating need to changes to gitignore

--- example.m
+++ example.m
@@ -6,14 +6,21 @@ int main(int argc, char **argv) {
 
   Interference * lineNoise = Interference.new;
 
-  while (argc > 1) { /// Parse options, with --multiline we enable multi line editing.
+  if (argc > 1) { /// Parse options
 
-    argc--; argv++;
+    NSArray *args = NSProcessInfo.processInfo.arguments;
 
-      !strcmp(*argv, "--multiline") ? [lineNoise setMultiLine:1], printf("Multi-line mode enabled.\n")
-    : !strcmp(*argv,  "--keycodes") ? [lineNoise  printKeyCodes], exit(0)
-    : fprintf(stderr, "Usage: %s [--multiline] [--keycodes]\n",
-             NSProcessInfo.processInfo.processName.UTF8String),   exit(1);
+    if ([args containsObject:@"--multiline"]) { ///  enable multi line editing.
+
+      int lines = [args[[args indexOfObject:@"--multiline"] + 1] integerValue];
+      [lineNoise setMultiLine:lines];
+      printf("Multi-line mode enabled (%i).\n", lines);
+
+    } else if ([args containsObject:@"--keycodes"])
+
+        [lineNoise  printKeyCodes], exit(0);
+
+    else { fprintf(stderr, "Usage: %s [--multiline] [--keycodes]\n", [args[0] UTF8String]); exit(1); }
   }
 
   /// Set the completion callback. This will be called every time the user uses the <tab> key.
@@ -23,11 +30,13 @@ int main(int argc, char **argv) {
     return [c isEqualToString:@"h"] ? @[@"hello", @"hello there"] : nil;
   }];
 
-  /// Load history from file. The history file is just a plain text file where entries are separated by newlines.
+  /// The history file is just a plain text file where entries are separated by newlines.
 
-  [lineNoise setHistoryLoadPath:@"/usr/share/dict/propernames"]; /// Load the history at startup
+  [lineNoise setHistoryLoadPath:@"/usr/share/dict/propernames"]; /// Loads this history file
 
-  [lineNoise setHistorySavePath:@"history_objc.txt"]; /// Load the history at startup
+  [lineNoise setHistorySavePath:@"history.txt "]; /// Optionally, set an alt. path to save history.
+
+  /// akin to example.c's main() function.
 
   [lineNoise prompt:@"linenoise > " withBlock:^BOOL(NSString *l) {
 
