a4b77a4c004f78c44bd76aae6476219adbf734fb
voldenet <voldenet@gmail.com>
2017-04-03 14:18:36 +0200

    add support for \e[1~ and \e[4~ escapes

--- linenoise.c
+++ linenoise.c
@@ -880,6 +880,12 @@ static int linenoiseEdit(int stdin_fd, int stdout_fd, char *buf, size_t buflen, 
                         case '3': /* Delete key. */
                             linenoiseEditDelete(&l);
                             break;
+                        case '1': /* Home */
+                            linenoiseEditMoveHome(&l);
+                            break;
+                        case '4': /* End */
+                            linenoiseEditMoveEnd(&l);
+                            break;
                         }
                     }
                 } else {
