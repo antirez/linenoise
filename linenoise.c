0cf125500f97bf1573665bf5de0cf95acf859ac5
Dima Krasner <dima@dimakrasner.com>
2017-08-29 19:00:02 +0300

    Ignored the return value of chmod()

--- linenoise.c
+++ linenoise.c
@@ -1170,11 +1170,7 @@ int linenoiseHistorySave(const char *filename) {
     fp = fopen(filename,"w");
     umask(old_umask);
     if (fp == NULL) return -1;
-    if (chmod(filename,S_IRUSR|S_IWUSR) < 0) {
-        fclose(fp);
-        unlink(filename);
-        return -1;
-    }
+    (void)chmod(filename,S_IRUSR|S_IWUSR);
     for (j = 0; j < history_len; j++)
         fprintf(fp,"%s\n",history[j]);
     fclose(fp);
