b9e97611eafd60c5df2ca7d0efbea827c5a2f61e
Sami Vaarala <sami.vaarala@iki.fi>
2017-11-04 01:52:23 +0200

    Fix a few more warnings

--- linenoise.c
+++ linenoise.c
@@ -1019,17 +1019,19 @@ static char *linenoiseNoTTY(void) {
     size_t len = 0, maxlen = 0;
 
     while(1) {
+        int c;
         if (len == maxlen) {
+            char *oldval;
             if (maxlen == 0) maxlen = 16;
             maxlen *= 2;
-            char *oldval = line;
+            oldval = line;
             line = realloc(line,maxlen);
             if (line == NULL) {
                 if (oldval) free(oldval);
                 return NULL;
             }
         }
-        int c = fgetc(stdin);
+        c = fgetc(stdin);
         if (c == EOF || c == '\n') {
             if (c == EOF && len == 0) {
                 free(line);
