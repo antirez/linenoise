ba628fda3f6e585a2026a4ba9a91a987ada92622
Francois Perrad <francois.perrad@gadz.org>
2019-09-08 09:14:41 +0200

    const variable

--- linenoise.c
+++ linenoise.c
@@ -117,7 +117,7 @@
 
 #define LINENOISE_DEFAULT_HISTORY_MAX_LEN 100
 #define LINENOISE_MAX_LINE 4096
-static char *unsupported_term[] = {"dumb","cons25","emacs",NULL};
+static const char * const unsupported_term[] = {"dumb","cons25","emacs",NULL};
 static linenoiseCompletionCallback *completionCallback = NULL;
 static linenoiseHintsCallback *hintsCallback = NULL;
 static linenoiseFreeHintsCallback *freeHintsCallback = NULL;
