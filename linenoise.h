a4254930ebef774c09e83b3ce1859d5d9f1561b5
Andrew V. Louis <13123840+avlouis@users.noreply.github.com>
2026-04-07 23:10:02 -0500

    Adjusting include guard macros to comply with standards. See Section 7.1.3 of https://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf or other editions of the C standard.

--- linenoise.h
+++ linenoise.h
@@ -36,8 +36,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __LINENOISE_H
-#define __LINENOISE_H
+#ifndef LINENOISE_H
+#define LINENOISE_H
 
 #ifndef LINENOISE_DEFAULT_HISTORY_MAX_LEN
 #define LINENOISE_DEFAULT_HISTORY_MAX_LEN 100
@@ -119,4 +119,4 @@ void linenoiseMaskModeDisable(void);
 }
 #endif
 
-#endif /* __LINENOISE_H */
+#endif /* LINENOISE_H */
